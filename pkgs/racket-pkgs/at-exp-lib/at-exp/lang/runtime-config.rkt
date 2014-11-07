e972753b59755c3a0ff457748371d48e0a37a5ed
AlexKnauth <alexander@knauth.org>
2014-11-06 22:42:25 -0500

    at-exp repl: update the current-readtable once
    instead of updating current-read-interaction to be a function that
    creates a new readtable every time
    see https://github.com/plt/racket/pull/811#issuecomment-61992444

--- pkgs/racket-pkgs/at-exp-lib/at-exp/lang/runtime-config.rkt
+++ pkgs/racket-pkgs/at-exp-lib/at-exp/lang/runtime-config.rkt
@@ -5,8 +5,4 @@
 (require (only-in scribble/reader make-at-readtable))
 
 (define (configure data)
-  (define old-read (current-read-interaction))
-  (define (new-read src in)
-    (parameterize ([current-readtable (make-at-readtable #:readtable (current-readtable))])
-      (old-read src in)))
-  (current-read-interaction new-read))
+  (current-readtable (make-at-readtable #:readtable (current-readtable))))
