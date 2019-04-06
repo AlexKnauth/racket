389aea4eff6b6274c17512db813ed1f664ac3096
AlexKnauth <alexander@knauth.org>
2019-04-06 14:16:37 -0400

    add ~dot-id template-metafunction
    for accessing nested attributes in macro-defining macros where the defined macro uses syntax-parse attributes

--- racket/collects/syntax/parse/experimental/template.rkt
+++ racket/collects/syntax/parse/experimental/template.rkt
@@ -1,5 +1,7 @@
 #lang racket/base
 (require (for-syntax racket/base)
+         racket/syntax
+         racket/string
          (only-in racket/private/template
                   metafunction))
 (provide (rename-out [syntax template]
@@ -34,3 +36,24 @@
     (unless (syntax? r)
       (raise-syntax-error #f "result of template metafunction was not syntax" stx))
     (old-mark (mark r))))
+
+;; ---------------------------
+
+;; #'(~dot-id a b c) = #'a.b.c
+
+(define-template-metafunction ~dot-id
+  (lambda (stx)
+    (syntax-case stx ()
+      [(_ x y ...)
+       (let ([ys (syntax->list #'(y ...))])
+         (define not-id
+           (ormap (λ (x) (and (not (identifier? x)) x)) (cons #'x ys)))
+         (when not-id
+           (raise-syntax-error #f "expected an identifier" stx not-id))
+         (format-id #'x "~a~a" #'x
+                    (string-append*
+                     (for/list ([y (in-list ys)])
+                       (format ".~s" (syntax-e y))))
+                    #:source stx
+                    #:props stx))])))
+
